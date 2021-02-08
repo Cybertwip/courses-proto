5dc71d568faa4953ae5be7d72be5d52d4064df36
Victor Lopez <vjlopezcarrillo@gmail.com>
2021-02-07 18:53:00 -0700

    First commit

---
+++ src/prismic-configuration.js
@@ -0,0 +1,20 @@
+import Prismic from 'prismic-javascript'
+
+const apiEndpoint = 'https://courses-proto.prismic.io/api/v2'
+
+// -- Access token if the Master is not open
+const accessToken = ''
+
+// OAuth
+// clientId: 'xxxxxx',
+// clientSecret: 'xxxxxx',
+
+// -- Links resolution rules
+// This function will be used to generate links to Prismic documents
+// As your project grows, you should update this function according to your routes
+export const linkResolver = (doc) => {
+  if (doc.type === 'post') return `/blog/${doc.uid}`
+  return '/'
+}
+
+export const client = Prismic.client(apiEndpoint, { accessToken })
